c1255e760732ca4eacbca166c2dcc167c33ef360
Alex <mysterybadger123456@gmail.com>
2026-09-12 00:50:04 -0500

    Queried todos in previous deployment, now changed to query sales table

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,20 @@ export default async function Page() {
   const cookieStore = await cookies()
   const supabase = createClient(cookieStore)
 
-  const { data: todos } = await supabase.from('todos').select()
+  const { data: sales, error } = await supabase.from('sales').select()
+
+  if (error) {
+    return <pre>{JSON.stringify(error, null, 2)}</pre>
+  }
+
+  if (sales?.length === 0) {
+    return <p>(sales table is empty)</p>
+  }
 
   return (
     <ul>
-      {todos?.map((todo) => (
-        <li key={todo.id}>{todo.name}</li>
+      {sales?.map((sale, i) => (
+        <li key={i}>{JSON.stringify(sale)}</li>
       ))}
     </ul>
   )
